80e959a9ae9f3809e61878aef331e5bf5b0f198e
J-P Stacey <me@jpstacey.info>
2010-04-27 19:09:34 +0100

    Making easy to split off a main.py

--- selenese.py
+++ selenese.py
@@ -35,52 +35,49 @@ def new_sel(domain="www.google.co.uk",start=False):
 class GenericTest(unittest.TestCase):
     """Container class for storing Selenium HTML converted tests"""
     def setUp(self):
-        self.verificationErrors = []
         self.selenium = new_sel(start = True)
         self.selenium.test = self
         self.mapper = SeleniumMapper(self)
 
     def tearDown(self):
         self.selenium.stop()
-        self.assertEqual([], self.verificationErrors)
-
-ConvertedTest = deepcopy(GenericTest)
 
 def convert_selenese(dir='.'):
     old_dir = os.getcwd()
     os.chdir(dir)
-    return old_dir
+
+    p = etree.HTMLParser()
+    x = etree.parse('index.html', p)
+    if DEBUG: print "Examining test suite: %s" % get_html_title(x)
+
+    class ConvertedTest(GenericTest): pass
+    i = 0
+    for test in x.findall("//table[@id='suiteTable']//tr//a"):
+        # Get HTML file for test
+        x_test = etree.parse(test.get("href"), p)
+        test_title = get_html_title(x_test)
+
+        # Find test table
+        test_table = x_test.find("/body//table")
+        if test_table != None:
+            if DEBUG: print "    Converting test: %s" % test_title
+
+            # Create a lambda function, give it a __doc__ so reporting is verbose
+            fn = lambda obj: run_html_test(test_title, test_table, obj)
+            fn.__setattr__("__doc__", test_title)
+            # Now add it to the ConvertedTest class as an object method test_N
+            type(ConvertedTest).__setattr__(ConvertedTest, "test_%d" % i, fn)
+            i+=1
+
+    os.chdir(old_dir)
+    return ConvertedTest
 
 try:
     convert_dir = sys.argv[1]
     sys.argv.pop(1)
 except IndexError:
     convert_dir = "."
-old_dir = convert_selenese(convert_dir)
-
-p = etree.HTMLParser()
-x = etree.parse('index.html', p)
-if DEBUG: print "Examining test suite: %s" % get_html_title(x)
-
-i = 0
-for test in x.findall("//table[@id='suiteTable']//tr//a"):
-    # Get HTML file for test
-    x_test = etree.parse(test.get("href"), p)
-    test_title = get_html_title(x_test)
-
-    # Find test table
-    test_table = x_test.find("/body//table")
-    if test_table != None:
-        if DEBUG: print "    Converting test: %s" % test_title
-
-        # Create a lambda function, give it a __doc__ so reporting is verbose
-        fn = lambda obj: run_html_test(test_title, test_table, obj)
-        fn.__setattr__("__doc__", test_title)
-        # Now add it to the ConvertedTest class as an object method test_N
-        type(ConvertedTest).__setattr__(ConvertedTest, "test_%d" % i, fn)
-        i+=1
-
-os.chdir(old_dir)
+MyTest = convert_selenese(convert_dir)
 
 # Run test suite, picking up on ConvertedTest().test_N for all tests N
 unittest.main()
