47d1cad5ebb43b456179988cf83ef5787fc8dc8a
Mauro Amico <mauro@biodec.com>
2010-10-16 06:28:17 +0800

    main

--- selenese/main.py
+++ selenese/main.py
@@ -3,19 +3,20 @@
 import sys
 from selenese import convert_selenese, unittest
 
-try:
-    convert_dir = sys.argv[1]
-    sys.argv.pop(1)
-except IndexError:
-    convert_dir = "."
+def main():
+    try:
+        convert_dir = sys.argv[1]
+        sys.argv.pop(1)
+    except IndexError:
+        convert_dir = "."
+    try:
+        u = sys.argv[1]
+        sys.argv.pop(1)
+    except IndexError:
+        u = "localhost"
+    MyTest = convert_selenese(convert_dir, u)
+    # Run test suite, picking up on ConvertedTest().test_N for all tests N
+    unittest.main()
 
-try:
-    u = sys.argv[1]
-    sys.argv.pop(1)
-except IndexError:
-    u = "localhost"
-
-MyTest = convert_selenese(convert_dir, u)
-
-# Run test suite, picking up on ConvertedTest().test_N for all tests N
-unittest.main()
+if __name__ == "__main__":
+    main()
